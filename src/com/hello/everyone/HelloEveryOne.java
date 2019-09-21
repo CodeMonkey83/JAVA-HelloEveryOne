e1678a81b998ded5f72af52629ac711d3c48709f
dewytear <youngjinkwak@gmail.com>
2019-09-21 16:37:09 +0900

    20190921 test

--- src/com/hello/everyone/HelloEveryOne.java
+++ src/com/hello/everyone/HelloEveryOne.java
@@ -4,7 +4,7 @@ public class HelloEveryOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello EveryOne");
+		System.out.println("Hello EveryOne!!");
 		System.out.println("commit test");
 	}
 
