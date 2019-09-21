4c313c5086e4cd4d9c0beb880872b54e7ef0b9ae
qjssmd83 <qjssmd83@gmail.com>
2019-09-21 16:20:54 +0900

    20190921 commit test

---
+++ src/com/hello/everyone/HelloEveryOne.java
@@ -0,0 +1,10 @@
+package com.hello.everyone;
+
+public class HelloEveryOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello EveryOne");
+	}
+
+}
